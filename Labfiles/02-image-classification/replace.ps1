18f767ad056cd85eefae8ada93acaa1b88abd8d3
John Kutama <105648214+JGDesigns2022@users.noreply.github.com>
2025-03-25 14:57:46 +0200

    aispho change

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<aisipho>'
+$storageAcct = 'aisipho'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
